bfda84eab134398f988a2f4624ab49a7d078c9bf
mad-alumno <mad-alumno@nm_aulas.com>
2019-06-06 11:38:57 +0200

    actualizacion paso test

--- src/main/java/es/orange/App.java
+++ src/main/java/es/orange/App.java
@@ -4,14 +4,20 @@ package es.orange;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    
-    public static int add(String nums){
-    	
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public static int add(String nums) {
+
+		String[] nums_arr = nums.split(",");
+		int suma = 0;
+		for (String unNum : nums_arr) {
+			suma += unNum.trim().equals("") ? 0 : Integer.parseInt(unNum.trim());
+		}
+		return suma;
+
+	}
+
 }
--- src/test/java/es/orange/AddTest.java
+++ src/test/java/es/orange/AddTest.java
@@ -7,8 +7,37 @@ import org.junit.Test;
 public class AddTest {
 
 	@Test
-	public void existAdd() {
-		fail("Not yet implemented");
+	public void existsAdd() {
+		App.add("");
 	}
 
+	@Test
+	public void sumavacio() {
+		assertTrue(App.add("") == 0);
+	}
+
+	@Test
+	public void sumaUno() {
+		assertTrue(App.add("1") == 1);
+	}
+
+	@Test
+	public void sumaDos() {
+		assertTrue(App.add("1,2") == 3);
+	}
+
+	@Test
+	public void sumaTres() {
+		assertTrue(App.add("1,2,3") == 6);
+	}
+	
+	@Test
+	public void sumaDosYVacio() {
+		assertTrue(App.add("1,2,") == 3);
+	}
+
+	@Test(expected = Exception.class)
+	public void sumaLetras() {
+		App.add("1,2,A");
+	}
 }
