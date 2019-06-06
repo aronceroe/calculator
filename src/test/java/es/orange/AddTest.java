f33fab6ca9cead064b62eda626106fdb46ce1793
mad-alumno <mad-alumno@nm_aulas.com>
2019-06-06 11:23:55 +0200

    primera carga

---
+++ src/test/java/es/orange/AddTest.java
@@ -0,0 +1,14 @@
+package es.orange;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddTest {
+
+	@Test
+	public void existAdd() {
+		fail("Not yet implemented");
+	}
+
+}
